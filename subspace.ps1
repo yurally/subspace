5e1c669ea1e789d4fd805c21fcc537df7de66f88
yurally <32533862+yurally@users.noreply.github.com>
2022-11-18 15:20:44 +0200

    Add files via upload

---
+++ subspace.ps1
@@ -0,0 +1,24 @@
+write-host "================================================================================"
+write-host "Enter the letter of the drive where you want to install Subspace. For example D"
+write-host "================================================================================"
+$disk=read-host
+write-host "==========================="
+write-host "Write the name of your node"
+write-host "==========================="
+$nodename=read-host
+write-host "==========================="
+write-host "Write plot size for farmer"
+write-host "==========================="
+$plotsize=read-host
+write-host "==========================="
+write-host "Enter your wallet address"
+write-host "==========================="
+$walletaddress=read-host
+mkdir ${disk}:\Subspace
+mkdir ${disk}:\Subspace\subspace-node
+mkdir ${disk}:\Subspace\subspace-farmer
+cd ${disk}:\Subspace
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-2a-2022-oct-06/subspace-farmer-windows-x86_64-gemini-2a-2022-oct-06.exe -OutFile subspace-farmer-windows-x86_64-gemini-2a-2022-oct-06.exe }"
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-2a-2022-oct-06/subspace-node-windows-x86_64-gemini-2a-2022-oct-06.exe -OutFile subspace-node-windows-x86_64-gemini-2a-2022-oct-06.exe }"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-node-windows-x86_64-gemini-2a-2022-oct-06.exe --chain gemini-2a --base-path ${disk}:\Subspace\subspace-node --execution wasm --state-pruning archive --validator --name $nodename"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-farmer-windows-x86_64-gemini-2a-2022-oct-06.exe --base-path D:\Subspace\subspace-farmer farm  --reward-address $walletaddress --plot-size $plotsize"
