78cbffb55d3c95184b332ef8c885c957f4971e8e
Yurii Bukshuk <32533862+yurally@users.noreply.github.com>
2023-05-05 20:23:07 +0300

    Update subspace_cli.sh

--- subspace_cli.sh
+++ subspace_cli.sh
@@ -41,7 +41,7 @@ systemctl stop subspaced
 rm $HOME/.config/subspace-cli/settings.toml
 mkdir $HOME/subspace; \
 cd $HOME/subspace && \
-wget https://github.com/subspace/subspace-cli/releases/download/v0.3.3-alpha/subspace-cli-ubuntu-x86_64-v2-v0.3.3-alpha -O subspace-cli && \
+wget https://github.com/subspace/subspace-cli/releases/download/v0.4.0-alpha/subspace-cli-ubuntu-x86_64-v2-v0.4.0-alpha -O subspace-cli && \
 sudo chmod +x subspace-cli && \
 sudo mv subspace-cli /usr/local/bin/ && \
 cd $HOME && \
@@ -81,7 +81,7 @@ apt install jq
 
 mkdir $HOME/subspace; \
 cd $HOME/subspace && \
-wget https://github.com/subspace/subspace-cli/releases/download/v0.3.3-alpha/subspace-cli-ubuntu-x86_64-v3-v0.3.3-alpha -O subspace-cli && \
+wget https://github.com/subspace/subspace-cli/releases/download/v0.4.0-alpha/subspace-cli-ubuntu-x86_64-v3-v0.4.0-alpha -O subspace-cli && \
 sudo chmod +x subspace-cli && \
 sudo mv subspace-cli /usr/local/bin/ && \
 cd $HOME && \
