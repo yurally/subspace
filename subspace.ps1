bcd5b4d3e95a51266dda318a6392655b104a840a
Yurii Bukshuk <32533862+yurally@users.noreply.github.com>
2023-07-03 21:55:12 +0300

    Update subspace.ps1

--- subspace.ps1
+++ subspace.ps1
@@ -18,8 +18,8 @@ mkdir ${disk}:\Subspace
 mkdir ${disk}:\Subspace\subspace-node
 mkdir ${disk}:\Subspace\subspace-farmer
 cd ${disk}:\Subspace
-powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jun-30/subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe -OutFile subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe }"
-powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jun-30/subspace-node-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe -OutFile subspace-node-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe }"
-Start-Process PowerShell.exe -ArgumentList ".\subspace-node-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe --chain gemini-3e --base-path ${disk}:\Subspace\subspace-node --execution wasm --blocks-pruning archive --state-pruning archive --dsn-disable-private-ips --no-private-ipv4 --validator --name $nodename"
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jul-03/subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe -OutFile subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe }"
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3e-2023-jul-03/subspace-node-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe -OutFile subspace-node-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe }"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-node-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe --chain gemini-3e --base-path ${disk}:\Subspace\subspace-node --execution wasm --blocks-pruning archive --state-pruning archive --dsn-disable-private-ips --no-private-ipv4 --validator --name $nodename"
 Start-Sleep 20
-Start-Process PowerShell.exe -ArgumentList ".\subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jun-30.exe --base-path ${disk}:\Subspace\subspace-farmer farm  --disable-private-ips --reward-address $walletaddress --plot-size $plotsize"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-farmer-windows-x86_64-skylake-gemini-3e-2023-jul-03.exe --base-path ${disk}:\Subspace\subspace-farmer farm  --disable-private-ips --reward-address $walletaddress --plot-size $plotsize"
