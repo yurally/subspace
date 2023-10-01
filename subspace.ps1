fd96c1e4b89766a01b89bcc49dc986f096f28d49
Yurii Bukshuk <32533862+yurally@users.noreply.github.com>
2023-10-01 13:12:47 +0300

    Update subspace.ps1

--- subspace.ps1
+++ subspace.ps1
@@ -18,8 +18,8 @@ mkdir ${disk}:\Subspace
 mkdir ${disk}:\Subspace\subspace-node
 mkdir ${disk}:\Subspace\subspace-farmer
 cd ${disk}:\Subspace
-powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-29/subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe -OutFile subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe }"
-powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3f-2023-sep-29/subspace-node-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe -OutFile subspace-node-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe }"
-Start-Process PowerShell.exe -ArgumentList ".\subspace-node-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe --chain gemini-3f --base-path ${disk}:\Subspace\subspace-node --execution wasm --blocks-pruning 256 --state-pruning archive --no-private-ipv4 --validator --name $nodename"
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3f-2023-oct-01/subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe -OutFile subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe }"
+powershell -command "& { iwr https://github.com/subspace/subspace/releases/download/gemini-3f-2023-oct-01/subspace-node-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe -OutFile subspace-node-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe }"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-node-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe --chain gemini-3f --base-path ${disk}:\Subspace\subspace-node --execution wasm --blocks-pruning 256 --state-pruning archive --no-private-ipv4 --validator --name $nodename"
 Start-Sleep 300
-Start-Process PowerShell.exe -ArgumentList ".\subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-sep-29.exe farm --reward-address $walletaddress path=${disk}:\Subspace\subspace-farmer,size=$plotsize"
+Start-Process PowerShell.exe -ArgumentList ".\subspace-farmer-windows-x86_64-skylake-gemini-3f-2023-oct-01.exe farm --reward-address $walletaddress path=${disk}:\Subspace\subspace-farmer,size=$plotsize"
